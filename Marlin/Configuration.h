cb0fb1ada8b635636638fb5cbe73189400f37f2f
3D-N00B <126980360+3D-N00B@users.noreply.github.com>
2023-03-16 18:34:33 -0300

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1757,7 +1757,7 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 180
+#define X_BED_SIZE 200
 #define Y_BED_SIZE 180
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
