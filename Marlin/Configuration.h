e0a97dafdaf9347320becb4a22ee56384b4b797c
3D-N00B <126980360+3D-N00B@users.noreply.github.com>
2023-03-17 11:00:10 -0300

    cfg

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1231,7 +1231,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 5120, 185.2 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 100 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
